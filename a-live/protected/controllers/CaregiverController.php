0ad8767117e2098944705b11548ae1c36aa1eccf
Kevin Palis <kevin.palis@gmail.com>
2013-05-06 01:24:23 +0800

    Set auth for other controllers to use Rights

--- a-live/protected/controllers/CaregiverController.php
+++ a-live/protected/controllers/CaregiverController.php
@@ -1,6 +1,6 @@
 <?php
 
-class CaregiverController extends Controller
+class CaregiverController extends RController
 {
 	/**
 	 * @var string the default layout for the views. Defaults to '//layouts/column2', meaning
@@ -14,16 +14,18 @@ class CaregiverController extends Controller
 	public function filters()
 	{
 		return array(
-			'accessControl', // perform access control for CRUD operations
+			//'accessControl', // perform access control for CRUD operations
+			'rights', //use RIGHTS access control filters
 		);
 	}
+
 
 	/**
 	 * Specifies the access control rules.
 	 * This method is used by the 'accessControl' filter.
 	 * @return array access control rules
 	 */
-	public function accessRules()
+	/*public function accessRules()
 	{
 		return array(
 			array('allow',  // allow all users to perform 'index' and 'view' actions
@@ -42,7 +44,7 @@ class CaregiverController extends Controller
 				'users'=>array('*'),
 			),
 		);
-	}
+	}*/
 
 	/**
 	 * Displays a particular model.
